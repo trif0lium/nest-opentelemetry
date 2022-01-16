9f57a436952567317837908877e42b890a440e98
trif0lium <gh@fleeingsunlight.dev>
2022-01-16 18:34:33 +0700

    feat(tracing/tracing): remove PinoInstrumentation

--- genshin-impact-api/libs/tracing/src/tracing/tracing.service.ts
+++ genshin-impact-api/libs/tracing/src/tracing/tracing.service.ts
@@ -1,15 +1,14 @@
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { InstrumentationOption } from '@opentelemetry/instrumentation'
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
 import { Resource } from '@opentelemetry/resources'
-import { SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/tracing'
+import { SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
 import { trace, context, Tracer, Span } from '@opentelemetry/api'
 import { TracingOptions, TRACING_OPTIONS } from './tracing.constant';
-import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+import { InstrumentationOption } from '@opentelemetry/instrumentation';
 
 
 @Injectable()
@@ -20,7 +19,6 @@ export class TracingService implements OnModuleInit {
     const instrumentations: InstrumentationOption[] = [
       new ExpressInstrumentation(),
       new HttpInstrumentation(),
-      new PinoInstrumentation()
     ]
 
     if (this.tracingOptions.serviceName !== 'api-gateway') {
