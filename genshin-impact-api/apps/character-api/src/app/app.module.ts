cf05e0ed6816896a6a551adbfdf8d16be368a42a
trif0lium <gh@fleeingsunlight.dev>
2022-01-16 11:53:13 +0700

    feat(character-api/app): add TracingModule with serviceName set to `character-api`

--- genshin-impact-api/apps/character-api/src/app/app.module.ts
+++ genshin-impact-api/apps/character-api/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TracingModule } from '@genshin-impact-api/tracing'
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLFederationModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    TracingModule.register({
+      serviceName: "character-api"
+    }),
+    GraphQLFederationModule.forRoot({
+      autoSchemaFile: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
