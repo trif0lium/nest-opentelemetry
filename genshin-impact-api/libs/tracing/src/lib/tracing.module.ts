dc34addef08c3d3c2a8b0a37b6b49eac10a27bc4
trif0lium <gh@fleeingsunlight.dev>
2022-01-16 11:33:17 +0700

    feat(tracing): register()

--- genshin-impact-api/libs/tracing/src/lib/tracing.module.ts
+++ genshin-impact-api/libs/tracing/src/lib/tracing.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { TracingOptions, TRACING_OPTIONS } from '../tracing/tracing.constant';
+import { TracingService } from '../tracing/tracing.service';
 
 @Module({
   controllers: [],
   providers: [],
   exports: [],
 })
-export class TracingModule {}
+export class TracingModule {
+  static register(options: TracingOptions): DynamicModule {
+    return {
+      module: TracingModule,
+      providers: [
+        {
+          provide: TRACING_OPTIONS,
+          useValue: options
+        },
+        TracingService
+      ],
+      exports: [TracingService]
+    }
+  }
+}
