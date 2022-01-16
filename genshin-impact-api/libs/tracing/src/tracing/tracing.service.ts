9121f1b97b294950d55fe30d49db1ca880cd9818
trif0lium <gh@fleeingsunlight.dev>
2022-01-16 11:46:54 +0700

    feat(tracing/tracing): register server-related instrumentation

--- genshin-impact-api/libs/tracing/src/tracing/tracing.service.ts
+++ genshin-impact-api/libs/tracing/src/tracing/tracing.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { NodeTracerProvider } from '@opentelemetry/node';
+import { Resource } from '@opentelemetry/resources'
+import { SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/tracing'
+import { TracingOptions, TRACING_OPTIONS } from './tracing.constant';
 
 @Injectable()
-export class TracingService {}
+export class TracingService implements OnModuleInit {
+  constructor(@Inject(TRACING_OPTIONS) private tracingOptions: TracingOptions) {}
+
+  onModuleInit() {
+    registerInstrumentations({
+      instrumentations: [
+        new HttpInstrumentation(),
+        new ExpressInstrumentation(),
+        new GraphQLInstrumentation()
+      ]
+    })
+
+    const provider = new NodeTracerProvider({
+      resource: Resource.default().merge(new Resource({
+        "service.name": this.tracingOptions.serviceName
+      }))
+    })
+
+    const consoleExporter = new ConsoleSpanExporter()
+    provider.addSpanProcessor(
+      new SimpleSpanProcessor(consoleExporter)
+    )
+
+    provider.register()
+  }
+}
